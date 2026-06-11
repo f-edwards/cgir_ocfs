be3d507a57044e1bb6bfec038afa8f1bd0cbf79f
Frank <frank.edwards@rutgers.edu>
2026-06-11 11:10:30 -0400

    mid-stream june update

--- sim_ocfs_data.r
+++ sim_ocfs_data.r
@@ -1,134 +1,103 @@
-set.seed(1)
 library(tidyverse)
-### define inverse inverse logit
-inv.logit <- function(x){
-  return(
-    exp(x)/(1 + exp(x))
-  )
-}
-
 # read in empirical distributions
 # scale down w1 to six months
 # delete after adjusting descriptive tables
-dat <- read_csv("./ocfs_output/child_period_bin.csv") 
+dat <- read_csv("./output1_12_26/child_table_period.csv")
 # pull IDs
-
-ID_list <- read_csv("./consort_data/group_assignment.csv") |> 
+ID_list <- read_csv("./consort_data/group_assignment.csv") |>
   rename(GROUP = Group)
-
-trt_ids <- ID_list |> 
-  filter(GROUP=="T")
-ctrl_ids <- ID_list |> 
-  filter(GROUP=="C")
 
 ### set up fake data for model diagnostics
 counties <- c("Monroe", "Onondaga", "Westchester")
-# sim child and hh 
+# sim child and hh
 ## county error
 county_error <- data.frame(
   County = counties,
-  county_error = rnorm(3, 0, 2))
+  county_error = rnorm(3, 0, 2)
+)
 ## household error
-hh_error <- ID_list |> 
-  mutate(hh_error = rnorm(nrow(ID_list), 0, 2),
-         County = sample(counties, nrow(ID_list), replace = T)) 
+# hh_error <- ID_list |>
+#   ,
+#          County = sample(counties, nrow(ID_list), replace = T))
 
-## child error and ID
-trt_child <- data.frame(Child = 1:350,
-                        GROUP = "T",
-                        ID = sample(trt_ids$ID, 350, replace = T),
-                        child_error = rnorm(350, 0, 2)) |> 
-  left_join(hh_error) |> 
-  left_join(county_error) |> 
-  mutate(total_error = county_error + hh_error + child_error)
+### FIX THIS SO EACH HH HAS CORRECT N OF CHILDREN
+w1 <- read_csv("./data/w1_reduced.csv") |>
+  mutate(COUNTY = str_sub(County, 1, -8)) |>
+  select(ID, COUNTY, n_child) |>
+  left_join(ID_list) |>
+  filter(!(is.na(GROUP)))
 
-c_child <- data.frame(Child = 350:1000,
-                      GROUP = "C",
-                      ID = sample(ctrl_ids$ID, 651, replace = T),
-                      child_error = rnorm(651, 0, 2)) |> 
-  left_join(hh_error) |> 
-  left_join(county_error) |> 
-  mutate(total_error = county_error + hh_error + child_error)
+# ok loop over each hh, populate child id, then randomly sample to n
+child_list <- list()
+child_index <- 1
+for (i in unique(w1$ID)) {
+  temp <- w1 |>
+    filter(ID == i)
+  if (temp$n_child > 0) {
+    child_ids <- data.frame(
+      Child = child_index:(child_index + temp$n_child - 1),
+      ID = temp$ID,
+      County = temp$COUNTY,
+      GROUP = temp$GROUP,
+      hh_error = rnorm(1, 0, 2)
+    )
+    child_index <- max(child_ids$Child) + 1
+    child_list[[i]] <- child_ids
+  }
+}
+child_ids <- bind_rows(child_list)
 
-sim_dat <- bind_rows(trt_child, c_child)
+sim_dat <- child_ids |>
+  mutate(child_error = rnorm(1012, 0, 2)) |>
+  left_join(county_error) |>
+  mutate(total_error = county_error + hh_error + child_error) |>
+  expand_grid(Wave = 1:4)
 
 # allocation ------------------------------------------------------------------
-# subset to wave
-sims <- list()
-for(i in 1:4){
-  dat_w <- dat |> 
-    filter(Wave == i) |> 
-    filter(.imp==1)
-  # sim total reports bounded by empirical
-  total_trt <- sample(trt_child$Child, 
-                         as.numeric(dat_w[dat_w$GROUP=="T", "TotalCPS_bin"]), 
-                         replace = F,
-                         prob = inv.logit(trt_child$total_error)) 
-  total_c <- sample(c_child$Child, 
-                       as.numeric(dat_w[dat_w$GROUP=="C", "TotalCPS_bin"]), 
-                       replace = F,
-                       prob = inv.logit(c_child$total_error)) 
-  total <- c(total_trt, total_c)
-  # sim confirmed 
-  confirmed_trt <- sample(trt_child$Child, 
-                      as.numeric(dat_w[dat_w$GROUP=="T", "Confirmed_bin"]), 
-                      replace = F,
-                      prob = inv.logit(trt_child$total_error)) 
-  confirmed_c <- sample(c_child$Child, 
-                    as.numeric(dat_w[dat_w$GROUP=="C", "Confirmed_bin"]), 
-                    replace = F,
-                    prob = inv.logit(c_child$total_error)) 
-  confirmed <- c(confirmed_trt, confirmed_c)
-  # sim prev
-  prev_trt <- sample(trt_child$Child, 
-                      as.numeric(dat_w[dat_w$GROUP=="T", "Prev_bin"]), 
-                      replace = F,
-                      prob = inv.logit(trt_child$total_error)) 
-  prev_c <- sample(c_child$Child, 
-                    as.numeric(dat_w[dat_w$GROUP=="C", "Prev_bin"]), 
-                    replace = F,
-                    prob = inv.logit(c_child$total_error)) 
-  prev <- c(prev_trt, prev_c)
-  # sim FC
-  FC_trt <- sample(trt_child$Child, 
-                     as.numeric(dat_w[dat_w$GROUP=="T", "FC_bin"]), 
-                     replace = F,
-                     prob = inv.logit(trt_child$total_error)) 
-  FC_c <- sample(c_child$Child, 
-                   as.numeric(dat_w[dat_w$GROUP=="C", "FC_bin"]), 
-                   replace = F,
-                   prob = inv.logit(c_child$total_error)) 
-  FC <- c(FC_trt, FC_c)
-  
-  sims[[i]] <- sim_dat |> 
-    mutate(Wave = i,
-           TotalCPS = Child%in%total,
-           Confirmed = Child%in%confirmed,
-           Prev = Child%in%prev,
-           FC = Child%in%FC)
-}
+# define lambda
+lambda <- dat |>
+  select(GROUP, Wave, n, TotalCPS, Confirmed, Prev, FC) |>
+  mutate(across(TotalCPS:FC, function(x) x / n)) |>
+  rename(
+    TotalCPS_l = TotalCPS,
+    Confirmed_l = Confirmed,
+    Prev_l = Prev,
+    FC_l = FC
+  ) |>
+  select(Wave, GROUP, TotalCPS_l:FC_l)
 
-c_long <- bind_rows(sims) |> 
-  rename(COUNTY = County)
+# initialize df
+sim_dat <- sim_dat |>
+  left_join(lambda) |>
+  mutate(
+    TotalCPS_l = TotalCPS_l * (1 + total_error / 10),
+    Confirmed_l = Confirmed_l * (1 + total_error / 10),
+    Prev_l = Prev_l * (1 + total_error / 10),
+    FC_l = FC_l * (1 + total_error / 10)
+  )
 
-### check variance of errors
-# c_long |> 
-#   summarize(c_sd = sd(county_error))
-# c_long |> 
-#   group_by(COUNTY) |> 
-#   summarize(hh_sd = sd(hh_error),
-#             child_sd = sd(child_error))
-# c_long |> 
-#   group_by(ID) |> 
-#   summarize(child_sd = sd(child_error)) |> 
-#   summarize(mean_sd = mean(child_sd))
-# looks reasonable
+# simulate
+sim_dat <- sim_dat |>
+  mutate(
+    TotalCPS = rpois(
+      n = nrow(sim_dat),
+      lambda = TotalCPS_l
+    ),
+    Confirmed = rpois(
+      n = nrow(sim_dat),
+      lambda = Confirmed_l
+    ),
+    Prev = rpois(
+      n = nrow(sim_dat),
+      lambda = Prev_l
+    ),
+    FC = rpois(
+      n = nrow(sim_dat),
+      lambda = FC_l
+    )
+  )
 
 # remove errors, output data in admin format
-c_long <- c_long |> 
-  select(Child:ID, COUNTY, Wave:FC)
-
-## add no_baseline_flag in to 6 cases
-ids_no_baseline <- sample(ID_list$ID, 6)
-c_long <- c_long |> 
-  mutate(NoBaseline_FLAG = ID%in%ids_no_baseline)
+c_long <- sim_dat |>
+  select(Child:GROUP, Wave, TotalCPS:FC) |>
+  rename(COUNTY = County)
