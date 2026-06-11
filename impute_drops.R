9b71392bdaa22486a28a8ad8d7bda4402363e703
Frank <frank.edwards@rutgers.edu>
2026-06-11 11:10:30 -0400

    mid-stream june update

--- impute_drops.R
+++ impute_drops.R
@@ -5,23 +5,33 @@
 library(tidyverse)
 library(mice)
 set.seed(1)
-# data read ---------------------------------------------------------------
-# source("sim_ocfs_data.r")
-
+ID_list <- read_csv("./data/group_assignment.csv") |>
+  rename(GROUP = Group)
 # create list of PINs in data
-ID_list <- c_long |> 
-  select(ID) |> 
+ID_list <- c_long |>
+  select(ID) |>
   distinct()
 
 # read wave 1 survey, return subset of useful variables for imputation
-w1 <- read_csv("./data/w1_reduced.csv") |> 
-  mutate(COUNTY = str_sub(County, 1, -8)) |> 
-  select(ID, COUNTY, unit, n_child,
-         Age_c, Gender_s, Race_White, Race_Black, Edu, SexOrient_Straight, 
-         SF_PhysicalFunctioning, SF_RoleLimitationsPhys,
-         Kessler_Score:Chaos_Score,
-         AH_Total:ParentalStressTotal) |> 
-  mutate(Gender_f = Gender_s == "Female") |> 
+w1 <- read_csv("./data/w1_reduced.csv") |>
+  mutate(COUNTY = str_sub(County, 1, -8)) |>
+  select(
+    ID,
+    COUNTY,
+    unit,
+    n_child,
+    Age_c,
+    Gender_s,
+    Race_White,
+    Race_Black,
+    SexOrient_Straight,
+    SF_PhysicalFunctioning,
+    SF_RoleLimitationsPhys,
+    Edu,
+    Kessler_Score:Chaos_Score,
+    AH_Total:ParentalStressTotal
+  ) |>
+  mutate(Gender_f = Gender_s == "Female") |>
   select(-Gender_s)
 
 # read IDs of drops
@@ -30,7 +40,7 @@ drops <- read_csv("./data/drops.csv")
 # prep df for drops -------------------------------------------------------
 # read
 extras <- w1 |>
-  filter(ID%in%drops$ID)
+  filter(ID %in% drops$ID)
 
 # create empty DF for children of drops
 extras_temp <- data.frame(
@@ -38,51 +48,51 @@ extras_temp <- data.frame(
   ID = rep(extras$ID, extras$n_child),
   COUNTY = rep(extras$COUNTY, extras$n_child),
   GROUP = rep(extras$unit, extras$n_child),
-  NoBaseline_FLAG = 0,
   TotalCPS = NA,
   Confirmed = NA,
   Prev = NA,
-  FC = NA)
+  FC = NA
+)
 
 # four waves
-extras_df <- extras_temp |> 
-  mutate(Wave = 1) |> 
-  bind_rows(extras_temp |> 
-              mutate(Wave = 2)) |> 
-  bind_rows(extras_temp |> 
-              mutate(Wave = 3)) |> 
-  bind_rows(extras_temp |> 
-              mutate(Wave = 4)) |> 
-  select(Child, GROUP, ID, COUNTY, Wave, 
-         NoBaseline_FLAG, 
-         TotalCPS:FC)
+extras_df <- extras_temp |>
+  mutate(Wave = 1) |>
+  bind_rows(
+    extras_temp |>
+      mutate(Wave = 2)
+  ) |>
+  bind_rows(
+    extras_temp |>
+      mutate(Wave = 3)
+  ) |>
+  bind_rows(
+    extras_temp |>
+      mutate(Wave = 4)
+  ) |>
+  select(Child, GROUP, ID, COUNTY, Wave, TotalCPS:FC)
 
 # bind
-temp <- c_long |> 
+temp <- c_long |>
+  mutate(Child = as.character(Child)) |>
   bind_rows(extras_df)
 
 # attach survey, prefer admin county classification
-temp <- temp |> 
-  left_join(w1 |> 
-              select(-COUNTY)) |> 
-  mutate(COUNTY = factor(COUNTY), 
-         GROUP = factor(GROUP),
-         Wave = factor(Wave),
-         Edu = factor(Edu))
+temp <- temp |>
+  left_join(
+    w1 |>
+      select(-COUNTY)
+  ) |>
+  mutate(COUNTY = factor(COUNTY), GROUP = factor(GROUP), Wave = factor(Wave))
 
 # imputation --------------------------------------------------------------
 # setup predictor matrix
 predmat <- make.predictorMatrix(temp)
 # turn off IDs
-predmat[,"Child"] <- 0
-predmat[,"ID"] <- 0
-predmat[,"unit"] <- 0
-predmat[,"NoBaseline_FLAG"] <- 0
-temp_imp <- mice(temp, 
-                   predictorMatrix = predmat,
-                   m = 5)
+predmat[, "Child"] <- 0
+predmat[, "ID"] <- 0
+predmat[, "unit"] <- 0
+predmat[, "Edu"] <- 0
+predmat["Edu", ] <- 0
+c_imp <- mice(temp, predictorMatrix = predmat, m = 5, maxit = 20)
 
-# output
-saveRDS(temp_imp, file = "imputed_data_DO_NOT_SEND_ME.rds")
-
-
+c_long_imp <- mice::complete(c_imp, action = "long", include = T)
--- make_all.R
+++ make_all.R
@@ -14,38 +14,21 @@ library(mice)
 # email contents of output to FE when complete
 
 # data input --------------------------------------------------------------
-# for FE, simulation, for HT read_csv
+# for FE simulation
 source("sim_ocfs_data.R")
-# make_c_long will pivot wide to long
-source("make_c_long.r")
+
+# FOR OCFS DATA make_c_long will pivot wide to long
+# cdat <-  read_csv("FILENAME.csv")
+# source("make_c_long.r")
 
 # imputation --------------------------------------------------------------
-# first time, source to generate imputed data from withdrawals. 
+# first time, source to generate imputed data from withdrawals.
 # will add 19 x 4 rows to c_long for missing cases
 # after one pass, use readRDS to save time
-if(!(file.exists("imputed_data_DO_NOT_SEND_ME.rds"))){
-  source("./impute_drops.R")
-} 
-
-c_imp <- readRDS("imputed_data_DO_NOT_SEND_ME.rds")
-c_long_imp <- mice::complete(c_imp,
-                              action = "long",
-                              include = T)
+source("./impute_drops.R")
 
 # descriptives ------------------------------------------------------------
 source("make_descriptives.R")
 
-### ADD A BIVAR THING, MAKE SOME SCATTERPLOTS OR SOMETHING
-### FOR prognostics:outcomes, demographics:outcomes
-
 # model estimation --------------------------------------------------------
-
-# frequentist models
-source("fit_models_R1_freq.R")
-
-# if frequentist models throw warnings, go Bayesian
-# can handle the more complex multilevel structure with child:hh nesting
-# bayesian models
-# will take ~6 hours to execute
-source("fit_models_R1_bayes.R")
-
+source("fit_models.R")
